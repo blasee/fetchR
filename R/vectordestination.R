d3761f43e1a9e4ba55474b67652dc7683691a9c4
blasee <blake.seers@gmail.com>
2014-08-29 11:34:00 +1200

    change assignment operator to =

--- R/vectordestination.R
+++ R/vectordestination.R
@@ -1,27 +1,27 @@
 vector_destination = function(lonlatpoint, travelvector) {
-  Rearth <- 6372795
-  Dd <- travelvector$magnitude / Rearth
-  Cc <- travelvector$direction
+  Rearth = 6372795
+  Dd = travelvector$magnitude / Rearth
+  Cc = travelvector$direction
   
   if (inherits(lonlatpoint, "SpatialPoints")) {
-    lata <- coordinates(lonlatpoint)[1, 2] * (pi / 180)
-    lona <- coordinates(lonlatpoint)[1, 1] * (pi / 180)
+    lata = coordinates(lonlatpoint)[1, 2] * (pi / 180)
+    lona = coordinates(lonlatpoint)[1, 1] * (pi / 180)
   }
   else {
-    lata <- lonlatpoint[2] * (pi / 180)
-    lona <- lonlatpoint[1] * (pi / 180)
+    lata = lonlatpoint[2] * (pi / 180)
+    lona = lonlatpoint[1] * (pi / 180)
   }
-  latb <- asin(cos(Cc) * cos(lata) * sin(Dd) + sin(lata) 
+  latb = asin(cos(Cc) * cos(lata) * sin(Dd) + sin(lata) 
                * cos(Dd))
-  dlon <- atan2(cos(Dd) - sin(lata) * sin(latb), sin(Cc) 
+  dlon = atan2(cos(Dd) - sin(lata) * sin(latb), sin(Cc) 
                 * sin(Dd) * cos(lata))
-  lonb <- lona - dlon + pi / 2
+  lonb = lona - dlon + pi / 2
   
-  lonb[lonb >  pi] <- lonb[lonb >  pi] - 2 * pi
-  lonb[lonb < -pi] <- lonb[lonb < -pi] + 2 * pi
+  lonb[lonb >  pi] = lonb[lonb >  pi] - 2 * pi
+  lonb[lonb < -pi] = lonb[lonb < -pi] + 2 * pi
   
-  latb <- latb * (180 / pi)
-  lonb <- lonb * (180 / pi)
+  latb = latb * (180 / pi)
+  lonb = lonb * (180 / pi)
   
   data.frame(longitude = lonb, latitude = latb)
 }
