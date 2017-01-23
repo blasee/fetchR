d605a8f364f76ad66aac6f06839167379f91aa3e
blasee <blake.seers@gmail.com>
2017-01-24 01:04:59 +1300

    Rewrite the fetch calculation algorithm using gIntersection from the sp package. This is much more accurate and efficient

--- R/fetch.R
+++ R/fetch.R
@@ -1,31 +1,27 @@
-#' Calculate Fetch for a (NZ) Coastal Marine Location
+#' Calculate Fetch for a New Zealand Coastal Marine Location
 #' 
-#' Calculate average fetch for a New Zealand coastal location given in decimal 
-#' degrees. The accuracy can be as low as 1m (0.001km) but can be very 
-#' computationally expensive, especially for exposed sites. The number of angles 
-#' can be between 4 and 100, however large values can be very computationally 
-#' expensive and may not improve the accuracy of the estimate too much. When 
-#' land is hit, the distance to coast from the previous iteration is used i.e. 
-#' the points will always end up in the water.
+#' Fetch is the unobstructed length of water over which wind can blow, and is
+#' commonly used as a measure of exposure to wind and waves at coastal sites. The 
+#' \code{fetch} function calculates the fetch for a New Zealand coastal location 
+#' for a number of bearings to quantify the fetch in 
+#' various compass directions.
 #' 
-#' @param lon longitude in decimal degrees.
 #' @param lat latitude in decimal degrees.
-#' @param max_dist maximum distance (km) allowed for any given angle (default 
-#'                 300).
-#' @param accuracy accuracy (km) of the fetch estimate (default 0.1km).
-#' @param degree_int interval of the directions for the fetch calculation in degrees 
-#'                   (equally spaced).
-#' @param quiet \code{FALSE}. Suppress diagnostic messages.
+#' @param lon longitude in decimal degrees.
+#' @param max_dist maximum distance in kilometers (default 300).
+#' @param n_bearings the number of bearings per quadrant. A value of 9 (default)
+#'                   gives a total of 36 bearings i.e. one at every 10 degrees.
+#' @param quiet suppress diagnostic messages (default \code{FALSE}).
+#' @param accuracy Deprecated.
+#' @param degree_int Deprecated.
 #' 
-#' @return returns an invisible dataframe containing the resultant coordinates 
-#'         at the coastline, magnitude and direction.
-#' @importFrom sp CRS proj4string Polygons Polygon SpatialPolygons SpatialPoints 
-#'                over coordinates
-#' @import rgeos
+#' @return Returns a \code{\link{Fetch}} object.
+#' @importFrom sp SpatialPoints CRS over spTransform coordinates
+#' @importFrom rgeos gBuffer gIntersects gIntersection
 #' @seealso \code{\link{fetchR}}
 #' @examples
 #' # Calculate fetch for Kawau Bay
-#' kawau_bay = fetch(174.8, -36.4)
+#' kawau_bay = fetch(-36.4, 174.8)
 #' 
 #' # Show the distances for each direction and the resultant locations
 #' kawau_bay
@@ -36,11 +32,22 @@
 #' # Plot the vectors
 #' plot(kawau_bay)
 #' 
-#' # Output the file as a KML for further investigation in Google Earth
-#' save_kml(kawau_bay, "kawau_bay")
+#' \dontrun{
+#' # ---- Output to KML/KMZ
+#' # Install and load plotKML library
+#' if (!require(plotKML))
+#'   install.packages("plotKML")
+#' library(plotKML)
+#' 
+#' Create some labels indicating the fetch vector directions
+#' labs = sapply(slot(kawau_bay, "lines"), slot, "ID")
+#' 
+#' # Save 'kawau_bay.kml' to the current directory
+#' kml(kawau_bay, labels = labs)
+#' }
 #' @export
-fetch = function(lon, lat, max_dist = 300, accuracy = 0.1, degree_int = 10,
-                 quiet = FALSE){
+fetch = function(lat, lon, max_dist = 300, n_bearings = 9,
+                 quiet = FALSE, accuracy, degree_int){
   if (!is.numeric(lon) || !is.numeric(lat))
     stop("longitude and latitude must be numeric")
   
@@ -53,116 +60,115 @@ fetch = function(lon, lat, max_dist = 300, accuracy = 0.1, degree_int = 10,
   if (max_dist < 1 || max_dist > 500)
     stop("max_dist must be between 1 and 500 km")
   
-  if (!is.numeric(accuracy) || length(accuracy) != 1)
-    stop("accuracy must be a single number")
+  if (!is.numeric(n_bearings) || length(n_bearings) != 1)
+    stop("n_bearings must be a single integer")
+  n_bearings = round(n_bearings)
   
-  if (accuracy < .001 || accuracy > max_dist)
-    stop(paste("accuracy must be between 0.001 km and", max_dist))
-  
-  if (!is.numeric(degree_int) || length(degree_int) != 1)
-    stop("degree_int must be a single number")
-  
-  if (degree_int < 4 || degree_int > 100)
-    stop("degree_int must be between 4 and 100")
+  if (n_bearings < 1 || n_bearings > 20)
+    stop("n_bearings must be between 1 and 20")
   
   if (!is.logical(quiet) || length(quiet) != 1 || is.na(quiet))
     stop("quiet must be either TRUE or FALSE")
+  
+  if (!missing(accuracy))
+    warning("the accuracy argument has been deprecated due to an improved algorithm.")
+  
+  if (!missing(degree_int))
+    warning("the degree_int argument has been deprecated; please use n_bearings instead.")
     
-  centre_point = SpatialPoints(data.frame(lon, lat), 
-                               proj4string = CRS(proj4string(nz_coast)))
+  centre_point_latlon = SpatialPoints(data.frame(lon, lat),
+                                      CRS("+proj=longlat +datum=WGS84"))
+  
   if (!quiet)
-    message("checking coordinate is not on land")
+    message("Projecting location onto NZTM 2000 map projection")
   
-  if (!is.na(over(nz_coast, centre_point)) ||
-        !is.na(over(nz_islands, centre_point)))
+  centre_point_proj = spTransform(centre_point_latlon, CRS("+init=epsg:2193"))
+  
+  if (!quiet)
+    message("Checking coordinate is not on land")
+  
+  if (any(!is.na(over(coastal_nz, centre_point_proj))))
     stop("coordinate is on land")
-  
+
   max_dist = max_dist * 1000
-  accuracy = accuracy * 1000
-  outer_radii = seq(accuracy, max_dist, by = accuracy)
-  directions = seq(0, 2 * pi, by = 2 * pi / (360 / degree_int))
-  if (tail(directions, 1) == 2 * pi)
-    directions = head(directions, -1)
+  bearings = head(seq(0, 360, by = 360 / (n_bearings * 4)), -1)
+  # Rearrange sequence order to start at 90 degrees to match up with the output
+  # from gBuffer
+  bearings = unlist(split(bearings, bearings < 90), use.names = FALSE)
   
-  circle_vector = data.frame(direction = directions, magnitude = max_dist)
-  my_circle = vector_destination(c(lon, lat), circle_vector)
+  # Create polygon (approximating a circle) with a given radius. These vertices
+  # are used for creating the end points for the fetch bearings.
+  d_bff = gBuffer(centre_point_proj, width = 30000, quadsegs = n_bearings)
   
-  circle_lon_min = min(my_circle$lon)
-  circle_lon_max = max(my_circle$lon)
-  circle_lat_min = min(my_circle$lat)
-  circle_lat_max = max(my_circle$lat)
-  bound_matrix = matrix(c(circle_lon_min, circle_lat_min,
-                          circle_lon_min, circle_lat_max,
-                          circle_lon_max, circle_lat_max,
-                          circle_lon_max, circle_lat_min,
-                          circle_lon_min, circle_lat_min),
-                        ncol = 2, byrow = TRUE)
-  bound_poly_p = Polygon(bound_matrix)
-  bound_poly_ps = Polygons(list(bound_poly_p), 1)
-  bound_poly_sps = SpatialPolygons(list(bound_poly_ps), 
-                                   proj4string = CRS(proj4string(nz_coast)))
+  # Subset NZ to incorporate only polygons within the radius to speed up 
+  # computation times.
+  coastal_nz_subset = coastal_nz[which(!is.na(over(coastal_nz, d_bff))), ]
   
+  # Calculate end points at the maximum distances.
+  max_dist_endpoints = head(coordinates(d_bff@polygons[[1]]@Polygons[[1]]), -1)
   
-  in_radius_islands = which(!is.na(over(nz_islands, bound_poly_sps)))
-  nz_islands_subset = nz_islands[in_radius_islands, ]
-  in_radius_coast = which(!is.na(over(nz_coast, bound_poly_sps)))
-  nz_coast_subset = nz_coast[in_radius_coast, ]
+  # Create spatialLines object
+  fetch_sp_lines = create_sp_lines(centre_point_proj, max_dist_endpoints, bearings)
   
+  # Which fetch bearings hit land?
+  hit_land = !sapply(gIntersects(fetch_sp_lines, coastal_nz_subset,
+                                 byid = c(TRUE, FALSE), returnDense = FALSE), 
+                     is.null)
+  
+  # Calculate intersections and identify closest shoreline
+
   if (!quiet)
     message("calculating fetch")
+  ints = gIntersection(fetch_sp_lines, coastal_nz_subset, byid = c(TRUE, FALSE))
+
+  fetch_ends = max_dist_endpoints
+  fetch_ends[hit_land, ] = t(sapply(ints@lines, function(x){
+    coordinates(x)[[1]][1, ]
+  }))
   
-  initial_vector = numeric(length(directions))
-  end_points = data.frame(longitude = initial_vector,
-                          latitude = initial_vector,
-                          direction = initial_vector,
-                          distance = initial_vector)
-  rows = rownames(end_points)
-  for (i in seq_along(outer_radii)){
-    circle_vector = data.frame(direction = directions, 
-                               magnitude = outer_radii[i])
-    my_circle = vector_destination(c(lon, lat), circle_vector)
-    my_points = SpatialPoints(my_circle, 
-                              proj4string = CRS(proj4string(nz_coast)))
-    on_land =
-      !is.na(as.character(over(my_points, nz_coast_subset)$name)) |
-      !is.na(as.character(over(my_points, nz_islands_subset)$name))
-    
-    if (any(on_land) || i == length(outer_radii)){
-      if (i == length(outer_radii)){
-        on_land = rep(TRUE, length(rows))
-        j = i
-      } else {
-        if (i == 1){
-          my_previous_circle = my_circle
-          warning(sum(on_land), " directions are less than ", accuracy, 
-                  "m from land", immediate. = TRUE)
-        }
-        else
-          j = i - 1
-      }
-      end_points[rows[on_land], ] = c(my_previous_circle$longitude[on_land],
-                                      my_previous_circle$latitude[on_land],
-                                      round(360 / (pi * 2) * 
-                                              directions[on_land], 2),
-                                      if (i == 1)
-                                        rep(0, sum(on_land))
-                                      else
-                                        outer_radii[rep(j, sum(on_land))] / 1000)
-      directions = directions[!on_land]
-      rows = rows[!on_land]
-      
-      if (length(rows) && !quiet)
-        message(length(rows), " more directions to calculate")
-    }
-    my_previous_circle = my_circle[!on_land, ]
-    if (!length(rows))
-      break
+  # Return the Fetch object
+  
+  new("Fetch",
+      location_lat = lat,
+      location_long = lon,
+      subset_map = coastal_nz_subset,
+      create_sp_lines(centre_point_proj, fetch_ends, bearings)
+  )
+}
+
+# Internal functions ------------------------------------------------------
+
+# Create SpatialLines objects
+#
+# This function takes a single location along with the end points for each 
+# direction and returns a SpatialLines object.
+# 
+# @param d the location of interest
+# @param end_points the end points for each direction of interest
+# @param bearings the directions to be used for the SpatialLines ID's
+#
+#' @importFrom sp CRS proj4string SpatialLines Lines Line
+
+create_sp_lines = function(d, end_points, bearings){
+  
+  # 1) Create the matrix of all the lons and lats
+  all_mat = rbind(do.call("rbind", rep(list(d@coords), nrow(end_points))), end_points)
+  
+  # 2) Split this matrix into a list
+  all_list = split(all_mat, 1:nrow(end_points))
+  
+  # 3) Create a list of 2x2 matrices indicating the start and end lons and lats
+  all_list_mat = lapply(all_list, matrix, ncol = 2)
+  
+  # 4) Create a list of Line objects
+  line_list = lapply(all_list_mat, Line)
+  
+  # 5) Create a list of Lines objects with different ID's
+  lines_list = vector("list", length(line_list))
+  for (i in seq_along(line_list)){
+    lines_list[[i]] = Lines(list(line_list[[i]]), ID = bearings[i])
   }
   
-  new("fetch", 
-      location_lat = lat, 
-      location_long = lon,
-      subset_coast = nz_coast_subset, 
-      subset_island = nz_islands_subset,
-      end_points)
+  # 6) Make it spatial
+  SpatialLines(lines_list, proj4string = CRS(proj4string(d)))
 }
--- R/fetch_class.R
+++ R/fetch_class.R
@@ -1,214 +1,166 @@
-load(system.file("extdata", "nz_coast.rda", package = "fetchR"))
-load(system.file("extdata", "nz_islands.rda", package = "fetchR"))
+load(system.file("extdata", "coastal_nz.rda", package = "fetchR"))
 
-#' Calculate Fetch with \pkg{fetchR}
+#' Calculate Wind Exposure with the \pkg{fetchR} Package
 #' 
-#' The \pkg{fetchR} package provides an objective calculation of fetch 
-#' based on current conventional standards.
+#' The \pkg{fetchR} package provides an objective calculation of wind fetch.
 #' 
-#' Fetch is an important measurement in coastal applications effectively 
-#' providing a proportional measurement of the length of water wind is blown 
-#' over. The higher the fetch the more energy is imparted to the surface of the 
+#' Fetch is an important measurement in coastal applications, which
+#' provides a measurement for the unobstructed length of water that wind from a
+#' certain direction can blow over. The higher the wind fetch from a certain bearing,
+#'  the more energy is imparted to the surface of the 
 #' water resulting in a larger sea state
-#' (\url{http://en.wikipedia.org/wiki/Fetch_(geography)}).
+#' (\url{http://en.wikipedia.org/wiki/Fetch_(geography)}). Therefore, the larger
+#' the fetch, the larger the exposure to wind and the more likely the coastal
+#' site experiences large sea states.
 #' 
-#' Fetch is currently calculated as the sum of distances from the marine 
-#' location to the shoreline for every 10 degrees (default) of the compass rose. 
-#' The only function required for the calculation of fetch at given latitude and
-#' longitude coordinates is \code{\link{fetch}}.
+#' Fetch from all directions can therefore be averaged to provide an overall 
+#' indication of the location's exposure to wind. The default is to provide the
+#' lengths (km) for every 10 degree bearings on the NZTM 2000 projection.
+#' Details of the New Zealand Transverse Mercator 2000 map projection can be found
+#' at 
+#' \url{http://www.linz.govt.nz/data/geodetic-system/datums-projections-and-heights/projections/new-zealand-transverse-mercator-2000}.
+#' 
+#' The \pkg{fetchR} package contains data sourced from Land Information New Zealand under CC-By.
 #' 
 #' @note The \pkg{fetchR} package is intended for New Zealand coastal
-#' applications and only supports marine sites within the following bounding 
+#' applications and only supports the specification of areas within the following bounding 
 #' box:
 #' 
 #' \tabular{lll}{
 #'  \tab Min \tab Max \cr
-#' Longitude \tab 167 \tab  178 \cr
-#' Latitude  \tab -47 \tab  -35 \cr
+#' Longitude \tab 165.869 \tab  183.846 \cr
+#' Latitude  \tab -52.6209 \tab  -29.2313 \cr
 #' }
 #' 
-#' Locations near the perimiter or outside this bounding box are either not 
-#' coastal or do not (yet?) have the high resolution coastal boundaries to 
-#' calculate the fetch.
-#' 
-#' @seealso \code{\link{fetch}}
+#' @seealso \code{\link{fetch}} for calculating fetch.
 #' @name fetchR
 #' @aliases fetchR-package
 #' @docType package
 #' @keywords package
 NULL
 
-#' Fetch Class
+#' Fetch Object
+#' 
+#' Extends the \code{\link[sp]{SpatialLines}} class to include the subset of the 
+#' coastal NZ map which can speed up plotting.
 #' 
 #' @slot location_lat latitude of the location of interest.
 #' @slot location_lon longitude of the location of interest.
-#' @slot subset_coast a subset of the NZ coast shapefile to increase efficiency.
-#' @slot subset_island a subset of the NZ offshore islands shapefile to increase
-#'                     efficiency.
+#' @slot subset_map a subset of the coastal NZ shapefile to increase efficiency when plotting.
 #' 
+#' @aliases Fetch
 #' @importFrom methods setClass
-#' @importClassesFrom sp SpatialPolygonsDataFrame
-setClass("fetch", 
+#' @importClassesFrom sp SpatialPolygonsDataFrame SpatialLines
+setClass("Fetch", 
          slots = c(location_lat = "numeric",
                    location_long = "numeric",
-                   subset_coast = "SpatialPolygonsDataFrame",
-                   subset_island = "SpatialPolygonsDataFrame"),
-         contains = "data.frame")
+                   subset_map = "SpatialPolygonsDataFrame"),
+         contains = "SpatialLines")
 
 #' Summarise a Fetch Object
 #' 
-#' The \code{summary} function calculates the mean and median fetch and the most 
-#' exposed direction(s).
+#' The \code{summary} function calculates the mean and median fetch of the 
+#' location, along with the average fetch for the separate northerly, easterly, 
+#' southerly and westerly quadrants. The mean fetch for the northerly component,
+#' for example, averages over the fetch vectors between directions 315 (inclusive)
+#' and 45 (exclusive), i.e. the fetch vectors within the interval [315, 45). 
+#' The number of bearings per quadrant (\code{n_bearings}) is also returned to 
+#' remind the user how many vectors were used for calculating the means for each 
+#' quadrant.
 #' 
-#' @param object \code{fetch} object as returned by \code{\link{fetch}}.
+#' @param object \code{Fetch} object as returned by \code{\link{fetch}}.
 #' 
 #' @importFrom methods setMethod
-#' @aliases summary,fetch-method
-#' @rdname summary
-#' @name summary
+#' @aliases summary,Fetch-method
+#' @importFrom sp SpatialLinesLengths
+#' @importFrom stats median
+#' @importFrom methods slot
 #' @export
-setMethod("summary", "fetch", function(object){
-  which_max = which(object$distance == max(object$distance))
+setMethod("summary", "Fetch", function(object){
+  angles = as.numeric(sapply(slot(object, "lines"), slot, "ID"))
+  angles_bin = findInterval(angles, seq(45, 315, by = 90))
+  angles[angles_bin == 4] = 0
   cat(
-    "Latitude: ", object@location_lat, "\n",
+    "Latitude:  ", object@location_lat, "\n",
     "Longitude: ", object@location_long, "\n",
-    "Average fetch:\t", mean(object$distance), "\n",
-    "Median fetch:\t", median(object$distance), "\n",
-    "Exposed directions:\t", paste(object$direction[which_max], collapse = ", "), 
-    "\n", sep = "")
+    "Average:   ", round(mean(SpatialLinesLengths(object)/1000), 2), "km\n",
+    "Median:    ", round(median(SpatialLinesLengths(object)/1000), 2), "km\n\n",
+    "Average northerly fetch [315, 45):  ", 
+    round(mean(SpatialLinesLengths(object)[angles_bin == 0] / 1000), 1), "km\n",
+    "Average easterly fetch [45, 135):   ", 
+    round(mean(SpatialLinesLengths(object)[angles_bin == 1] / 1000), 1), "km\n",
+    "Average southerly fetch [135, 225): ", 
+    round(mean(SpatialLinesLengths(object)[angles_bin == 2] / 1000), 1), "km\n",
+    "Average westerly fetch [225, 315):  ", 
+    round(mean(SpatialLinesLengths(object)[angles_bin == 3] / 1000), 1), "km\n\n",
+    "n_bearings = ", table(angles_bin)[[1]], "\n", sep = "")
 })
 
 #' @aliases fetch
 #' @rdname fetch
+#' @param object a \code{\link{Fetch}} object.
 #' @importFrom methods setMethod
+#' @importFrom sp spTransform SpatialLinesLengths
 #' @export
-setMethod("show", "fetch", function(object){
-  print(object)
+setMethod("show", "Fetch", function(object){
+  obj_latlon = spTransform(object, CRS("+proj=longlat +datum=WGS84"))
+  lat_lon_mat = t(sapply(sapply(slot(obj_latlon, "lines"), slot, "Lines"), 
+                         slot, "coords"))[, c(4, 2)]
+  unord_df = data.frame(latitude = lat_lon_mat[, 1],
+                        longitude = lat_lon_mat[, 2],
+                        direction = as.numeric(
+                          sapply(slot(object, "lines"), slot, "ID")),
+                        fetch = round(SpatialLinesLengths(object) / 1000, 2))
+  ord_df = unord_df[order(unord_df$direction), ]
+  rownames(ord_df) = NULL
+  print(ord_df)
 })
 
 #' Plot a Fetch Object
 #' 
-#' This is the default plot method for a \code{\link{fetch}} object. A map is
-#' plotted with the area determined by the length of the direction vectors. The 
+#' This is the default plot method for a \code{\link{Fetch}} object. The NZ
+#' Transverse Mercator 2000 projection is used for the plot and the fetch 
 #' vectors are plotted as lines originating from the location's coordinates.
 #' 
-#' @param x \code{fetch} object as returned by \code{\link{fetch}}.
+#' @param x \code{\link{Fetch}} object as returned by \code{\link{fetch}}.
 #' @param y missing (not used).
-#' @param ... further arguments passed to \code{\link{segments}}
+#' @param ... further arguments passed to \code{\link[sp]{plot,SpatialLines,missing-method}}.
 #' 
-#' @importFrom methods setMethod
-#' @aliases plot,fetch,missing-method
-#' @rdname plot
-#' @name plot
+#' @importFrom methods setMethod as
+#' @importFrom sp SpatialLines
 #' @export
-setMethod("plot", c("fetch", "missing"), function(x, y, ...){
-  x0 = x@location_long
-  y0 = x@location_lat
-  x1 = x$longitude
-  y1 = x$latitude
-  message("initializing plot...")
-  dev.hold()
-  plot(x[, 1:2], type = "n")
-  plot(x@subset_coast, add = TRUE, col = "grey")
-  plot(x@subset_island, add = TRUE, col = "grey")
-  segments(x0, y0, x1, y1, ...)
-  invisible(dev.flush())
+setMethod("plot", c("Fetch", "missing"), function(x, y, ...){
+  plot(as(x, "SpatialLines"), ...)
+  plot(x@subset_map, add = TRUE, col = "lightgrey")
 })
 
+#' Deprecated Functions in \pkg{fetchR}
+#' 
+#' \code{save_kml} has been deprecated. Instead use the \code{kml} function in
+#' the \pkg{plotKML} package.
+#' 
+#' @usage
+#' 
+#' @examples 
+#' \dontrun{
+#' kawau_bay = fetch(-36.4, 174.8)
+#' 
+#' # Install plotKML if required
+#' if (!require(plotKML))
+#'   install.packages("plotKML")
+#'   
+#' library(plotKML)
+#' 
+#' # Create some labels indicating the fetch vector directions
+#' labs = sapply(slot(kawau_bay, "lines"), slot, "ID")
+#' 
+#' # Save 'kawau_bay.kml' to the current directory
+#' kml(kawau_bay, labels = labs)
+#' }
+#' 
+#' @name fetchR-deprecated
 #' @aliases save_kml
-#' @rdname save_kml
-#' @importFrom methods setGeneric
-setGeneric("save_kml", function(object, file_name) {
-  standardGeneric("save_kml")
-})
-
-#' Save a KML file of the direction vectors
-#' 
-#' The \code{save_kml} function saves a KML file for use in Google Earth or 
-#' other KML-friendly software.
-#' 
-#' This function is useful for checking that no vectors pass through any 
-#' subtidal reefs or other potentially fetch-limiting structures that aren't 
-#' defined in the original shape file.
-#' 
-#' @param object a \code{fetch} object
-#' @param file_name character string giving the KML file name. The \code{.kml}
-#'                  suffix is not required.
-#' 
-#' @importFrom methods setMethod
-#' @importFrom XML newXMLDoc newXMLNode saveXML
-#' @rdname save_kml
-#' @aliases save_kml
-#' @name save_kml
 #' @export
-setMethod(
-  "save_kml", 
-  signature(object = "fetch", file_name = "ANY"), 
-  function(object, file_name){
-    if (missing(file_name))
-      stop("file_name must be supplied")
-    if (!is.character(file_name))
-      stop("file_name must be a character string")
-    file_name = file_name[1]
-    doc = newXMLDoc()
-    kml.node = 
-      newXMLNode("kml", 
-                 doc = doc, 
-                 namespaceDefinitions = 
-                   c("http://www.opengis.net/kml/2.2", 
-                     gx = "http://www.google.com/kml/ext/2.2", 
-                     kml = "http://www.opengis.net/kml/2.2", 
-                     atom = "http://www.w3.org/2005/Atom"))
-    Document.node = newXMLNode("Document", 
-                               parent = kml.node)
-    newXMLNode("name", strsplit(file_name, ".kml")[[1]],
-               parent = Document.node)
-    newXMLNode("StyleMap", attrs = c(id = "mouseover"),
-               parent = Document.node,
-               newXMLNode("Pair",
-                          newXMLNode("key", "normal"),
-                          newXMLNode("styleUrl", "#normal")),
-               newXMLNode("Pair",
-                          newXMLNode("key", "highlight"),
-                          newXMLNode("styleUrl", "#highlight")))
-    newXMLNode("Style", attrs = c(id = "highlight"),
-               parent = Document.node,
-               newXMLNode("LineStyle",
-                          newXMLNode("color", "FF1400FF"),
-                          newXMLNode("width", "3")))
-    newXMLNode("Style", attrs = c(id = "normal"),
-               parent = Document.node,
-               newXMLNode("LineStyle",
-                          newXMLNode("color", "50146AFF"),
-                          newXMLNode("width", "2")))
-    
-    
-    fetch_folder = newXMLNode("Folder", 
-                              parent = Document.node,
-                              newXMLNode("name", "Fetch"),
-                              newXMLNode("open", "1"))
-    for (i in seq_along(object$distance))
-      newXMLNode("Placemark",
-                 newXMLNode("name", paste(object$direction[i], "degrees")),
-                 newXMLNode("description", paste(object$distance[i], "km")),
-                 newXMLNode("styleUrl", "#mouseover"),
-                 newXMLNode("LineString",
-                            newXMLNode("tessellate", "1"),
-                            newXMLNode("coordinates", 
-                                       paste0(object@location_long, ",",
-                                             object@location_lat, ",",
-                                             "0", " ",
-                                             object$longitude[i], ",",
-                                             object$latitude[i], ",",
-                                             "0"))), 
-                 parent = fetch_folder)
-    
-    if (grepl("\\.kml$", file_name))
-      xml_file = file_name
-    else
-      xml_file = paste0(file_name, ".kml")
-    
-    saveXML(doc, file = xml_file)
-    message(paste("output KML file:", xml_file))
-  }
-)
+save_kml = function(object, file_name){
+  .Deprecated("kml", "fetchR")
+}
